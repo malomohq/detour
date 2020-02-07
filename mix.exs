6f659996327feada8b59efe5dea0a01e114c4956
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-07 09:45:20 -0500

    Auto-start application

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule Detour.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: { Detour.Application, [] }
     ]
   end
 
