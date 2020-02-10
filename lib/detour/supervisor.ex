a77df3303fdfa0df04d25edcc74530ca3e36f1ac
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-10 17:44:12 -0500

    Allow supervisor naming

--- lib/detour/supervisor.ex
+++ lib/detour/supervisor.ex
@@ -7,14 +7,18 @@ defmodule Detour.Supervisor do
 
   @spec start_child(Keyword.t()) :: DynamicSupervisor.on_start_child()
   def start_child(opts) do
+    supervisor = Keyword.get(opts, :supervisor)
+
     spec = %{ id: Detour.Server, start: { Detour.Server, :start_link, [opts] } }
 
-    DynamicSupervisor.start_child(__MODULE__, spec)
+    DynamicSupervisor.start_child(supervisor, spec)
   end
 
-  @spec start_link :: Supervisor.on_start()
-  def start_link do
-    DynamicSupervisor.start_link(__MODULE__, :ok, name: Detour.Supervisor)
+  @spec start_link(Keyword.t()) :: Supervisor.on_start()
+  def start_link(opts \\ []) do
+    name = Keyword.get(opts, :name, __MODULE__)
+
+    DynamicSupervisor.start_link(__MODULE__, :ok, name: name)
   end
 
   #
