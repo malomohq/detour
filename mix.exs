e7a4462ade904067e28f6c3f119afca04dc7a8c7
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-10 21:07:46 -0500

    Added ex_doc as a dev dependency

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Detour.MixProject do
       # test
       #
 
-      { :dialyxir, "~> 1.0.0-rc", only: [:dev], runtime: false }
+      { :dialyxir, "~> 1.0.0-rc", only: [:dev], runtime: false },
+      { :ex_doc, ">= 0.0.0", only: [:dev], runtime: false }
     ]
   end
 
