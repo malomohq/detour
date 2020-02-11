b0b04389776d02fa42ce948f3d817c4162238950
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-10 20:57:00 -0500

    Allow usage of Elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Detour.MixProject do
     [
       app: :detour,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
